5efb28266b773566b14325a91b7347ae9ef8959d
Zinevich Yan <facecrack1337@gmail.com>
2021-03-28 23:36:28 +0500

    :sparkles: Add body wrapper

--- src/components/ui/main/Main.tsx
+++ src/components/ui/main/Main.tsx
@@ -12,12 +12,21 @@ const Wrapper = styled.div`
     color: ${(props) => props.theme.colors.background.main.contrast};
 `;
 
+const BodyWrapper = styled.div`
+    border-radius: 20px;
+    height: 100%;
+    width: 100%;
+    margin: 10px 0;
+`;
+
 export const Main: FC<MainProps> = ({
     children
 }: MainProps) => (
     <Wrapper>
         <Container>
-            {children}
+            <BodyWrapper>
+                {children}
+            </BodyWrapper>
         </Container>
     </Wrapper>
 )
