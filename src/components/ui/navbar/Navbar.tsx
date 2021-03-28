749eec4b9f7b643b8eccdc7fa976a0bbadc33150
Zinevich Yan <facecrack1337@gmail.com>
2021-03-28 20:32:24 +0500

    :boom: Add nav items

--- src/components/ui/navbar/Navbar.tsx
+++ src/components/ui/navbar/Navbar.tsx
@@ -1,6 +1,10 @@
-import { FC } from "react";
+import { FC, ReactElement } from "react";
 import styled from 'styled-components';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
+import {
+    Face,
+    AccountCircle,
+} from '@material-ui/icons';
 
 import { Container } from '../container';
 
@@ -11,7 +15,40 @@ type NavbarProps = {
 const Wrapper = styled.div`
     background: ${(props) => props.theme.colors.background.header.color};
     color: ${(props) => props.theme.colors.background.header.contrast};
+
+    > * {
+        height: 100%;
+        > * {
+            height: 100%;
+        }
+    }
 `;
+
+const NavbarWrapper = styled.div`
+    display: flex;
+    margin: auto 0;
+
+    > div {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+    }
+`;
+
+const LinkWrapper = styled.div`
+    margin: auto 0;
+
+    > * {
+        color: ${(props) => props.theme.colors.background.header.contrast};
+        text-decoration: none;
+        display: flex;
+        text-align: center;
+
+        > * {
+            margin: auto 0;
+        }
+    }
+`
 
 export const Navbar: FC<NavbarProps> = () => {
     const location = useLocation();
@@ -19,10 +56,46 @@ export const Navbar: FC<NavbarProps> = () => {
     return (
         <Wrapper>
             <Container>
-                <div>
-                    Nav bar
-                </div>
+                <NavbarWrapper>
+                    <div>
+
+                        <Route
+                            currentRoute={location.pathname}
+                            route="/groups"
+                        >
+                            <>
+                                <Face /> <span>Группы</span>
+                            </>
+                        </Route>
+
+                        <Route
+                            currentRoute={location.pathname}
+                            route="/account"
+                        >
+                            <>
+                                <AccountCircle /> <span>Аккаунт</span>
+                            </>
+                        </Route>
+
+                    </div>
+                </NavbarWrapper>
             </Container>
         </Wrapper>
     );
 }
+
+type RouteProps = {
+    children: ReactElement;
+    currentRoute: string;
+    route: string;
+}
+
+const Route: FC<RouteProps> = ({
+    children,
+    currentRoute,
+    route
+}: RouteProps) => (
+    <LinkWrapper>
+        {currentRoute === route ? <div>{children}</div> : <Link to={route}> {children} </Link>}
+    </LinkWrapper>
+)
