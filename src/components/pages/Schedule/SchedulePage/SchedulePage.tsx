726359f8a283f8a04defa3af164e997ea2349348
Zinevich Yan <facecrack1337@gmail.com>
2021-03-31 02:40:42 +0500

    :tada: Add schedule

--- src/components/pages/Schedule/SchedulePage/SchedulePage.tsx
+++ src/components/pages/Schedule/SchedulePage/SchedulePage.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
 import { ShedulePageLink } from './styled';
 
+import { useServer } from '../../../../hooks';
+import { GetDetailedGroup, DetailedGroup } from '../../../../server';
 
 // type
 type SchedulePageParams = {
@@ -12,56 +13,36 @@ type SchedulePageProps = {}
 
 export const SchedulePage: React.FC<SchedulePageProps> = (props) => {
   const id = useParams<SchedulePageParams>().id;
+  const [group, setGroup] = useState<DetailedGroup>();
+
+  const detailed = useServer(GetDetailedGroup);
+
+  useEffect(() => {
+    detailed.fetch({
+      id: Number(id)
+    });
+  }, []);
+
+  const loading = detailed.state.fetching;
+  const success = detailed.state.answer.succeeded;
+
+  useEffect(() => {
+    if (success) {
+      setGroup(detailed.state.answer.data!);
+
+      detailed.reload();
+    }
+  }, [success]);
+
+  const toShow = group?.tasks.map(task => (
+    <ShedulePageLink to={`/schedule/${id}/${task.date}`} key={task.date}>
+      <h3>{task.date}</h3>
+    </ShedulePageLink>
+  ))
 
   return (
     <div>
-      <ShedulePageLink to={`/schedule/${id}/1`}>
-        <h3>Пн</h3>
-
-        <p>что-то важное на день 1</p>
-      </ShedulePageLink>
-      <ShedulePageLink to={`/schedule/${id}/2`}>
-        <div>
-          <h3>Вт</h3>
-
-          <p>что-то важное на день 2</p>
-        </div>
-      </ShedulePageLink>
-      <ShedulePageLink to={`/schedule/${id}/3`}>
-        <div>
-          <h3>Ср</h3>
-
-          <p>что-то важное на день 3</p>
-        </div>
-      </ShedulePageLink>
-      <ShedulePageLink to={`/schedule/${id}/4`}>
-        <div>
-          <h3>Чт</h3>
-
-          <p>что-то важное на день 4</p>
-        </div>
-      </ShedulePageLink>
-      <ShedulePageLink to={`/schedule/${id}/5`}>
-        <div>
-          <h3>Пт</h3>
-
-          <p>что-то важное на день 5</p>
-        </div>
-      </ShedulePageLink>
-      <ShedulePageLink to={`/schedule/${id}/6`}>
-        <div>
-          <h3>Сб</h3>
-
-          <p>что-то важное на день 6</p>
-        </div>
-      </ShedulePageLink>
-      <ShedulePageLink to={`/schedule/${id}/7`}>
-        <div>
-          <h3>Вс</h3>
-
-          <p>что-то важное на день 7</p>
-        </div>
-      </ShedulePageLink>
+      {toShow}
     </div>
   );
 };
